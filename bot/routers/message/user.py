15256b1373b924e79984f42cc0af4824e656fe34
welaskez <nazar.nikitenko.03@mail.ru>
2025-02-19 23:06:48 +0500

    feat: add base handlers for all msgs

--- bot/routers/message/user.py
+++ bot/routers/message/user.py
@@ -1,6 +1,7 @@
 import message_texts
 from aiogram import Router, types
 from aiogram.filters import Command, CommandStart
+from keyboards.inline import common
 from services.user import UserService
 
 router = Router(name=__name__)
@@ -14,3 +15,8 @@ async def handle_start(message: types.Message, user_service: UserService):
 @router.message(Command("help"))
 async def handle_help(message: types.Message):
     await message.answer(text=message_texts.HELP)
+
+
+@router.message()
+async def handle_message(message: types.Message):
+    await message.answer(text=message_texts.START, reply_markup=common.start_kb)
