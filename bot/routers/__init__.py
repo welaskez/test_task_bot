396007985316253cca2796408aad4eeb26d6c3f1
welaskez <nazar.nikitenko.03@mail.ru>
2025-02-19 22:41:17 +0500

    feat: create callback query router for user

--- bot/routers/__init__.py
+++ bot/routers/__init__.py
@@ -2,7 +2,9 @@ __all__ = ("router",)
 
 from aiogram import Router
 
+from .callback_query import router as callback_query_router
 from .message import router as message_router
 
 router = Router(name=__name__)
 router.include_router(message_router)
+router.include_router(callback_query_router)
