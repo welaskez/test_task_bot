8d7de84939300b2ffa247846ee582e74455e4241
welaskez <nazar.nikitenko.03@mail.ru>
2025-02-19 22:31:06 +0500

    feat: added start command handler

--- bot/routers/message/user.py
+++ bot/routers/message/user.py
@@ -1,3 +1,10 @@
-from aiogram import Router
+from aiogram import Router, types
+from aiogram.filters import CommandStart
+from services.user import UserService
 
 router = Router(name=__name__)
+
+
+@router.message(CommandStart())
+async def handle_start(message: types.Message, user_service: UserService):
+    await user_service.handle_start(message)
--- bot/services/user.py
+++ bot/services/user.py
@@ -1,5 +1,9 @@
+import message_texts
+from aiogram.types import Message
 from core.models import User
+from core.schemas.user import UserCreate
 from crud.user import UserCRUD
+from keyboards.inline import common
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .base import BaseService
@@ -9,3 +13,13 @@ class UserService(BaseService[User]):
     def __init__(self, session: AsyncSession) -> None:
         super().__init__(session, UserCRUD)
         self.crud: UserCRUD = self.crud
+
+    async def get_by_tg_id(self, tg_id: int) -> User | None:
+        return await self.crud.get_one_by_expression(User.tg_id == tg_id)
+
+    async def handle_start(self, message: Message) -> None:
+        user = await self.get_by_tg_id(message.from_user.id)
+        if not user:
+            await self.create(UserCreate(tg_id=message.from_user.id))
+
+        await message.answer(text=message_texts.START, reply_markup=common.start_kb)
