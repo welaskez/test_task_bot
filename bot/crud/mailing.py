47476393446772938dff23410e8355f4dca10f70
welaskez <nazar.nikitenko.03@mail.ru>
2025-02-20 13:57:09 +0500

    feat: added mailing crud & get scheduled mailings method

--- bot/crud/mailing.py
+++ bot/crud/mailing.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from core.models import Mailing
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from crud.base import BaseCRUD
@@ -9,3 +12,7 @@ class MailingCRUD(BaseCRUD[Mailing]):
 
     def __init__(self, session: AsyncSession) -> None:
         super().__init__(session)
+
+    async def get_scheduled_mailings(self) -> list[Mailing]:
+        stmt = select(Mailing).where(Mailing.time <= datetime.now())
+        return list(await self.session.scalars(stmt))
